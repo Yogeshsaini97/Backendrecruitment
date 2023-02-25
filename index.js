8c545dcc869afa6201555944e6b4f269a6dfa642
Yogesh saini <84733974+yogeshsaini1997@users.noreply.github.com>
2023-02-25 21:47:17 +0530

    changes

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 const Mongoose=require("mongoose")
 const express=require("express")
 const app=express();
@@ -6,16 +7,55 @@ const cors=require("cors")
 
 const runbackend= async ()=>
 {
-
-    let a=0
     
-//  Mongoose.connect("mongodb://0.0.0.0:27017/mydatabase",()=>
-// {
-//  console.log("connected to mongoose")
-//   a=1;
-// })
 
-  
+ 
+
+ 
+Mongoose.connect("mongodb://0.0.0.0:27017/mydatabase",()=>
+{
+ console.log("connected to mongoose")
+}); 
+
+
+
+const Companyschema=new Mongoose.Schema({
+   companyName: {
+       type: String,
+       required: true,
+      
+     },
+     Profiles: { type: Array, default: [] },
+     Location: {
+       type: Array, default: [] 
+ 
+     },
+     WorkingDays: {
+       type: String,
+      
+     
+     },
+     experienceRequired:{
+       type: String
+     },
+     CTC: {
+       type: String,
+       Default:"as per market standards"
+      
+     
+     },
+     Hremailid: {
+       type: String,
+      
+     
+     },
+
+},{timestamps:true});   
+
+const CompanyModel= await Mongoose.model("myJobs",Companyschema);
+
+
+
 
 
 
@@ -24,22 +64,47 @@ const PORT=5000;
 app.use(express.json());
 app.use(cors());
 
+console.log("hy")
+
+
+
+
+// app.post("/addcompany",async (req,res)=>
+// {
+  
+//     let data=await new CompanyModel(req.body); 
+//     console.log(data)
+//     const done=await data.save();
+//     console.log(data)
+//     res.send(req.body);
+//     console.log(req.body);
+
+
+// })
+
 app.get("/addcompany",async(req,resp)=>
 {
 
 // let data=await CompanyModel.find();
-resp.send(a);
-console.log(a)
+console.log("hy");
+resp.send("yooooo");
 
 })
 
-app.listen(PORT,()=>
+
+
+
+await app.listen(PORT,()=>
 {
    console.log(`connected to ${PORT}`)
 })
 
+
 }
 
-runbackend();
+
+
+runbackend()
+
 
 
